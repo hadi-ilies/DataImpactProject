e7631e5451d9113bd0eab3bd919eba1c47ffb21a
hadi bereksi <hadi-ilies.bereksi-reguig@epitech.eu>
2021-05-05 21:23:32 +0200

    feat(server): add deserialization concurrency

--- server/app/controllers/usercontrollers/user.go
+++ server/app/controllers/usercontrollers/user.go
@@ -3,8 +3,10 @@ package usercontrollers
 import (
 	"DataImpactProject/server/app/db"
 	"DataImpactProject/server/app/models/usermodels"
-	"fmt"
+	"encoding/json"
+	"io/ioutil"
 	"net/http"
+	"sync"
 
 	"github.com/gin-gonic/gin"
 	"golang.org/x/crypto/bcrypt"
@@ -13,19 +15,49 @@ import (
 
 //Create: The JSON input file must be serialized and then saved to a MongoDB database. The JSON file will contain an array of users. You will then have to serialize the data concurrently and then insert it into the database without processing the already inserted entries again.The password must be encrypted with bcrypt and the hash of it inserted into the database.In addition to inserting this data into the database, you will have to generate one file per entry with the entry id as the filename, this file should contain only the Data field
 func Create(ctx *gin.Context) {
-	var users []usermodels.User
-	err := ctx.Bind(&users)
+	//read json request
+	jsons, err := ioutil.ReadAll(ctx.Request.Body)
 	if err != nil {
-		fmt.Println("Error = ", err)
-		ctx.JSON(http.StatusBadRequest, gin.H{
-			"message": "Error while decoding",
+		ctx.JSON(http.StatusInternalServerError, gin.H{
+			"message": "Error while Reading request body",
 		})
 		return
 	}
+	//Note: I would use ctx.Bind this if the subject did not asked for desarialization concurrency
+	//parse json array partially
+	var serializedUser []interface{}
+	err = json.Unmarshal(jsons, &serializedUser)
+	if err != nil {
+		ctx.JSON(http.StatusInternalServerError, gin.H{
+			"message": "Error while Unmarshalling",
+		})
+		return
+	}
+	//init waitgroup
+	wait := new(sync.WaitGroup)
+	users := make([]usermodels.User, len(serializedUser))
 
+	for i, s := range serializedUser {
+		wait.Add(1)
+		//goroutine for concurent Deserialization
+		go func(j int, s interface{}) {
+			defer wait.Done()
+			deserializedObject, err := doDeserialization(s)
+			if err != nil {
+				ctx.JSON(http.StatusBadRequest, gin.H{
+					"message": "Error while decoding",
+				})
+				return
+			}
+			users[j] = deserializedObject
+		}(i, s)
+	}
+	wait.Wait()
+
+	//save in db
 	session, err := db.GetMongoSession()
 	if err != nil {
-		ctx.JSON(http.StatusBadRequest, gin.H{
+		ctx.JSON(http.StatusInternalServerError, gin.H{
 			"message": "Error while getting mongo session",
 		})
 		return
@@ -33,7 +65,7 @@ func Create(ctx *gin.Context) {
 	defer session.Close()
 	c := session.DB("dataImpact").C("users")
 	for _, user := range users {
-		//TODO check id user exist with this email
+		//check user email already exist
 		err = c.Find(bson.M{"email": user.Email}).One(&user)
 		if err == nil {
 			ctx.JSON(http.StatusBadRequest, gin.H{
@@ -50,22 +82,17 @@ func Create(ctx *gin.Context) {
 			})
 			return
 		}
+		//generate file with the user ID as filename and the data field as content
+		err = createFile(user.Id, []byte(user.Data))
+		if err != nil {
+			ctx.JSON(http.StatusInternalServerError, gin.H{
+				"message": "Error while creating data file",
+			})
+		}
 	}
-	ctx.JSON(http.StatusOK, users)
-	// wait := new(sync.WaitGroup)
-
-	// for i, user := range users {
-	// 	wait.Add(1)
-	// 	fmt.Println("user: ", i, " = ", user.Name)
-	//     go func(j int) {
-	// 		defer wait.Done()
-	// 		deserializedObject, err := doDeserialization(user)
-	// 		if err != nil {
-	// 			// add error handling here
-	// 		}
-	// 		objs[j] = deserializedObject
-	// 	}(j)
-	// }
+	ctx.JSON(http.StatusOK, gin.H{
+		"message": "users saved",
+	})
 }
 
 //Login: The user is able to connect to his profile in order to access all the data assigned to him. We consider that the user has an email and a password
@@ -78,15 +105,14 @@ func Login(ctx *gin.Context) {
 
 	err = ctx.Bind(&authUser)
 	if err != nil {
-		fmt.Println("Error = ", err)
-		ctx.JSON(http.StatusBadRequest, gin.H{
+		ctx.JSON(http.StatusInternalServerError, gin.H{
 			"message": "Error while decoding",
 		})
 		return
 	}
 	session, err := db.GetMongoSession()
 	if err != nil {
-		ctx.JSON(http.StatusBadRequest, gin.H{
+		ctx.JSON(http.StatusInternalServerError, gin.H{
 			"message": "Error while getting mongo session",
 		})
 		return
@@ -110,7 +136,6 @@ func Login(ctx *gin.Context) {
 	}
 	//account exist
 	validToken, err := getJWT()
-	fmt.Println(validToken)
 	//Note: it is not really nessesary to save the token but can be cool to keep it if I want to avoid sending ID in request in order to get/delete/update my user
 	user.Token = validToken
 	//update
@@ -128,6 +153,7 @@ func Login(ctx *gin.Context) {
 
 //Delete: delete a user with its Id, as well as the generated file (I'm doing as the subject want) but if we were in a real case. I would deleted the ID param from the request and I would allowed a user to delete ONLY his own account thx to his token
 func Delete(ctx *gin.Context) {
+	//check token
 	_, err := IsAuthorized(ctx.Request)
 
 	if err != nil {
@@ -139,7 +165,7 @@ func Delete(ctx *gin.Context) {
 
 	session, err := db.GetMongoSession()
 	if err != nil {
-		ctx.JSON(http.StatusBadRequest, gin.H{
+		ctx.JSON(http.StatusInternalServerError, gin.H{
 			"message": "Error while getting mongo session",
 		})
 		return
@@ -149,9 +175,15 @@ func Delete(ctx *gin.Context) {
 	userID := ctx.Param("id")
 	err = c.Remove(bson.M{"_id": userID})
 	if err != nil {
-		//TODO check status code
 		ctx.JSON(http.StatusNoContent, gin.H{
 			"message": "User does not exist",
+		})
+		return
+	}
+	err = deleteFile(userID)
+	if err != nil {
+		ctx.JSON(http.StatusInternalServerError, gin.H{
+			"message": "Error: while deleting the file",
 		})
 		return
 	}
@@ -162,6 +194,7 @@ func Delete(ctx *gin.Context) {
 
 //GetAllUsers: retrieve a list of users
 func GetAllUsers(ctx *gin.Context) {
+	//check token
 	_, err := IsAuthorized(ctx.Request)
 
 	if err != nil {
@@ -173,7 +206,7 @@ func GetAllUsers(ctx *gin.Context) {
 
 	session, err := db.GetMongoSession()
 	if err != nil {
-		ctx.JSON(http.StatusBadRequest, gin.H{
+		ctx.JSON(http.StatusInternalServerError, gin.H{
 			"message": "Error while getting mongo session",
 		})
 		return
@@ -193,6 +226,7 @@ func GetAllUsers(ctx *gin.Context) {
 
 //GetUserByID: retrieve a user
 func GetUserByID(ctx *gin.Context) {
+	//check token
 	_, err := IsAuthorized(ctx.Request)
 
 	if err != nil {
@@ -203,7 +237,7 @@ func GetUserByID(ctx *gin.Context) {
 	}
 	session, err := db.GetMongoSession()
 	if err != nil {
-		ctx.JSON(http.StatusBadRequest, gin.H{
+		ctx.JSON(http.StatusInternalServerError, gin.H{
 			"message": "Error while getting mongo session",
 		})
 		return
@@ -224,6 +258,7 @@ func GetUserByID(ctx *gin.Context) {
 
 //Update: modify a user with its id, if the data field changes the file must be modified
 func Update(ctx *gin.Context) {
+	//check token
 	_, err := IsAuthorized(ctx.Request)
 
 	if err != nil {
@@ -236,7 +271,7 @@ func Update(ctx *gin.Context) {
 	userID := ctx.Param("id")
 	session, err := db.GetMongoSession()
 	if err != nil {
-		ctx.JSON(http.StatusBadRequest, gin.H{
+		ctx.JSON(http.StatusInternalServerError, gin.H{
 			"message": "Error while getting mongo session",
 		})
 		return
@@ -244,28 +279,41 @@ func Update(ctx *gin.Context) {
 	defer session.Close()
 	c := session.DB("dataImpact").C("users")
 
-	var user usermodels.User
-	err = ctx.Bind(&user)
+	//get current user data
+	var currentUser usermodels.User
+	err = c.Find(bson.M{"_id": userID}).One(&currentUser)
 	if err != nil {
-		fmt.Println("Error = ", err)
+		ctx.JSON(http.StatusNoContent, gin.H{
+			"message": "userID does not exist",
+		})
+		return
+	}
+
+	//unmarshal data that will be updated
+	var updatedUser usermodels.User
+	err = ctx.Bind(&updatedUser)
+	if err != nil {
 		ctx.JSON(http.StatusBadRequest, gin.H{
 			"message": "Error while decoding",
 		})
 		return
 	}
-	err = c.Update(bson.M{"_id": userID}, user)
+	err = c.Update(bson.M{"_id": userID}, updatedUser)
 	if err != nil {
 		ctx.JSON(http.StatusNoContent, gin.H{
 			"message": "userID does not exist",
 		})
 		return
 	}
-	err = c.Find(bson.M{"_id": userID}).One(&user)
-	if err != nil {
-		ctx.JSON(http.StatusNoContent, gin.H{
-			"message": "userID does not exist",
-		})
-		return
+	//compare previous data field to the new one, If different we update the file otherwise we don't
+	if currentUser.Data != updatedUser.Data {
+		err = updateFile(userID, []byte(updatedUser.Data))
+		if err != nil {
+			ctx.JSON(http.StatusInternalServerError, gin.H{
+				"message": "Error While updating File",
+			})
+			return
+		}
 	}
-	ctx.JSON(http.StatusOK, user)
+	ctx.JSON(http.StatusOK, updatedUser)
 }
